3cbb9b0e59e80633acb61a79469ffd69c3669ffe
jjy_8888@163.com <jianyong.jiang@starlight-sms.com>
2026-02-02 18:47:23 +0800

    add md

---
+++ stock_analyze/analyze/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Google;
+
+#pragma warning disable SKEXP0070
+#pragma warning disable SKEXP0110
+IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+kernelBuilder.AddGoogleAIGeminiChatCompletion(
+    modelId: "gemini-3-flash-preview",
+    apiKey: "AIzaSyCUGzkEObs4mXozvcDTX0Yp6fKiKG2BFuY"
+);
+Kernel kernel = kernelBuilder.Build();
+
+ChatCompletionAgent agent =
+    new()
+    {
+        Name = "SK-Agent",
+        Instructions = "You are a helpful assistant.",
+        Kernel = kernel,
+    };
+
+await foreach (AgentResponseItem<ChatMessageContent> response
+    in agent.InvokeAsync("what's your name?"))
+{
+    Console.WriteLine(response.Message);
+}
+
+Console.ReadLine();
